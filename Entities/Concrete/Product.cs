c54cc2da4fff58c5c97e5e7964d924502788f46b
muhammed <muhammedcoban14@gamil.com>
2021-01-31 00:20:12 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //public clasa diğer katmanlarda ulaşabilsin demektir. public ile birlikte entities'i diger 3 katmanda kullanacak
+   public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+
+
+    }
+}
